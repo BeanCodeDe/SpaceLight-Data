8966d2060a659bb150b03bea441a85330c7fe5be
Niklas <n.nikisch@gmail.com>
2022-05-30 17:03:09 +0100

    add method to  update token

--- internal/auth/AuthFacade.go
+++ internal/auth/AuthFacade.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"net/http"
 	"os"
 	"time"
 
@@ -50,10 +49,6 @@ func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 		}
 
 		claims := &claims{}
-		if time.Now().Before(time.Unix(claims.ExpiresAt, 0)) {
-			log.Debugf("Token is passed expire date")
-			return echo.ErrUnauthorized
-		}
 		tkn, err := jwt.ParseWithClaims(authHeader, claims, func(token *jwt.Token) (interface{}, error) {
 			return []byte(secret), nil
 		})
@@ -67,52 +62,13 @@ func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 			return echo.ErrUnauthorized
 		}
 
-		token, err := CreateJWTToken(claims.UserId)
-		if err != nil {
-			return echo.ErrUnauthorized
-		}
-		c.Response().Header().Set(headerName, token)
-
-		return next(c)
-	}
-}
-
-func TokenRefresherMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		// If the user is not authenticated (no user token data in the context), don't do anything.
-		authHeader := c.Request().Header.Get(headerName)
-		if authHeader == "" {
-			return next(c)
-		}
-
-		claims := claims{}
-		// We ensure that a new token is not issued until enough time has elapsed.
-		// In this case, a new token will only be issued if the old token is within
-		// 15 mins of expiry.
-		if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) < 1*time.Minute {
-			// Gets the refresh token from the cookie.
-			// Parses token and checks if it valid.
-			tkn, err := jwt.ParseWithClaims(authHeader, claims, func(token *jwt.Token) (interface{}, error) {
-				return []byte(secret), nil
-			})
+		if time.Now().Add(1 * time.Minute).After(time.Unix(claims.ExpiresAt, 0)) {
+			token, err := CreateJWTToken(claims.UserId)
 			if err != nil {
-				if err == jwt.ErrSignatureInvalid {
-					c.Response().Writer.WriteHeader(http.StatusUnauthorized)
-				}
-				log.Debugf("Claim could not be parsed, %v", err)
+				return echo.ErrUnauthorized
 			}
-
-			if tkn != nil && tkn.Valid {
-				// If everything is good, update tokens.
-				token, err := CreateJWTToken(claims.UserId)
-				if err != nil {
-					c.Response().Writer.WriteHeader(http.StatusUnauthorized)
-				} else {
-					c.Response().Header().Set(headerName, token)
-				}
-			}
+			c.Response().Header().Set(headerName, token)
 		}
-
 		return next(c)
 	}
 }
